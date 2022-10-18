e70f2bc38c7dba3f5fcffdeabd55a8ccf9ce93b9
Sheseescheese? <79773348+INODGEEKZ@users.noreply.github.com>
2022-10-18 16:34:42 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ global.APIs = {
 
 // Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-	'https://alphabot-api.herokuapp.com': 'vW4N5VaO',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://alphabot-api.herokuapp.com': 'OmMAEccj',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
